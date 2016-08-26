38b96b8b4f5bf7866d4d47303edafde0cda99e74
Blake Contreras <shardsofsilver@gmail.com>
2016-08-26 11:56:02 -0700

    Add tests for Create view.

---
+++ test/unit/Create.spec.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Create from '../../components/Create.vue';
+
+describe('Create.vue', () => {
+  it('should have a title', () => {
+
+  })
+  it('should allow creation of multiple choice questions', () => {
+
+  })
+  it('should allow creation of fill-in questions', () => {
+
+  })
+  it('should allow creation of short answer questions', () => {
+
+  })
+  it('should allow creation of multiple questions', () => {
+
+  })
+  xit('should allow import of old quiz questions', () => {
+
+  })
+  xit('should allow comparison with old quizzes', () => {
+    
+  })
+})
