74c6e157deeef5e6f2b179f300d43f72b8b33625
Blake Contreras <shardsofsilver@gmail.com>
2016-08-26 11:54:50 -0700

    Render Quiz component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <quiz></quiz>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+import Quiz from './components/Quiz.vue'
+
+export default {
+  components: {
+    Hello,
+    Quiz
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
