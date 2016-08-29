37f921fe635e79a4f8e2795874b21ffd8f9df40e
Blake Contreras <shardsofsilver@gmail.com>
2016-08-29 16:57:07 -0700

    Add Create view to App for testing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <hello></hello>
-    <quiz></quiz>
+    <!--<quiz></quiz>-->
+    <create></create>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello.vue'
 import Quiz from './components/Quiz.vue'
+import Create from './components/Create.vue'
 
 export default {
   components: {
     Hello,
-    Quiz
+    Quiz,
+    Create
   }
 }
 </script>
--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="create-view">
     <div class="title"><h1>Create a Quiz</h1></div>
-    <div class="subheader description">Choose a quiz type: </div>
-    <div class="type-select">
+    <div class="type-select" v-show="unselected">Choose a quiz type:
       <form class="type-select-form">
-        <input type="radio" name="quiz-type" value="mc">Multiple Choice
-        <input type="radio" name="quiz-type" value="fi">Fill In
-        <input type="radio" name="quiz-type" value="sa">Short Answer
-        <input type="radio" name="quiz-type" value="mx">Mixed
-        <input type="button" name="select-type" value="Select" v-on:click="">
+        <input type="radio" name="quiz-type" value="mc" v-model="selected">Multiple Choice
+        <input type="radio" name="quiz-type" value="fi" v-model="selected">Fill In
+        <input type="radio" name="quiz-type" value="sa" v-model="selected">Short Answer
+        <input type="radio" name="quiz-type" value="mx" v-model="selected">Mixed
+        <input type="button" name="select-type" value="Select" v-on:click="handleSelect">
       </form>
     </div>
   </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        selected: null,
+        unselected: true,
+        renderComponent: null
+      }
+    },
+    methods: {
+      handleSelect() {
+        if (this.selected === null) {
+          alert("Make a selection first")
+        } else {
+          this.renderComponent = this.selected
+        }
+      }
+    }
+  }
   // select a quiz type
     // show form based on selection
       // forms: multiple choice, short answer, fill in, mixed
